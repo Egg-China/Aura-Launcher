4542bf1aa0fe990872fc3a9d31bdfb44a02d6e90
ACX <anxunbcx@gmail.com>
2026-08-27 21:31:30 +0800

    Fix runtime bridge code style

--- AuraLauncher/src/test/java/org/jackhuang/hmcl/plugin/bridge/RuntimeBridgeWireCodecTest.java
+++ AuraLauncher/src/test/java/org/jackhuang/hmcl/plugin/bridge/RuntimeBridgeWireCodecTest.java
@@ -6,6 +6,14 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package org.jackhuang.hmcl.plugin.bridge;
 
@@ -28,9 +36,9 @@ final class RuntimeBridgeWireCodecTest {
     @Test
     void encodesCanonicalIntegerFixture() throws IOException {
         assertArrayEquals(new byte[]{
-                        (byte) 0x92, 0x02, (byte) 0xd3,
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2a
-                },
+                (byte) 0x92, 0x02, (byte) 0xd3,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2a
+        },
                 RuntimeBridgeWireCodec.encode(BridgeValue.integer(42L)));
     }
 
